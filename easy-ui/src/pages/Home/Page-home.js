9051c3776774453701d0bfdcbe87e70890b5de75
VanChien411 <phuthienchien3@gmail.com>
2025-03-22 02:28:15 +0700

    create layout

---
+++ easy-ui/src/pages/Home/Page-home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function PageHome() {
+  return (
+    <main>
+      <header> Headerdfdg </header>
+
+      <article> Content </article>
+
+      <aside> Sidebar </aside>
+
+      <footer> Footer </footer>
+    </main>
+  );
+}
+
+export default PageHome;
