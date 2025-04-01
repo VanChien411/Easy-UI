4a7eca21fa3a5d07aa9cec111bf283ce71971262
VanChien411 <phuthienchien3@gmail.com>
2025-04-02 05:00:28 +0700

    add Alert component with toast notifications; update styles and integrate UUID for unique identifiers

--- easy-ui/src/components/ui/LoginSignup/LoginSignup.js
+++ easy-ui/src/components/ui/LoginSignup/LoginSignup.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom"; // Import useNavigate
 import { register, login } from "../../../services/userService";
 import Spinner from "../../utils/Spinner"; // Import the Spinner component
+import Alert, { showAlert } from "../../utils/Alert";
 import "../../../assets/styles/LoginSignup.css";
 
 function LoginSignup() {
   const { action } = useParams();
+  const navigate = useNavigate(); // Initialize navigate
   const containerRef = useRef(null);
   const registerBtnRef = useRef(null);
   const loginBtnRef = useRef(null);
@@ -13,10 +15,11 @@ function LoginSignup() {
     email: "",
     password: "",
     fullName: "",
-    phoneNumber: "",
   });
   const [rememberMe, setRememberMe] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [isPasswordTyped, setIsPasswordTyped] = useState(false);
 
   useEffect(() => {
     const container = containerRef.current;
@@ -70,6 +73,16 @@ function LoginSignup() {
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
+  const handlePasswordChange = (e) => {
+    const { value } = e.target;
+    setFormData((prev) => ({ ...prev, password: value }));
+    setIsPasswordTyped(value.length > 0);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   const handleRegister = async (e) => {
     e.preventDefault();
     setIsLoading(true);
@@ -78,11 +91,19 @@ function LoginSignup() {
         email: formData.email,
         password: formData.password,
         fullName: formData.fullName,
-        phoneNumber: formData.phoneNumber,
       });
-      alert("Registration successful!");
+      showAlert({
+        title: "Success",
+        message: "Registration successful!",
+        type: "success",
+      });
+      navigate("/"); // Redirect to homepage
     } catch (error) {
-      alert(error.message); // Display error message
+      showAlert({
+        title: "Error",
+        message: error.message,
+        type: "error",
+      });
     } finally {
       setIsLoading(false);
     }
@@ -103,9 +124,18 @@ function LoginSignup() {
         localStorage.removeItem("savedEmail");
         localStorage.removeItem("savedPassword");
       }
-      alert("Login successful!");
+      navigate("/"); // Redirect to homepage
+      // showAlert({
+      //   title: "Success",
+      //   message: "Login successful!",
+      //   type: "success",
+      // });
     } catch (error) {
-      alert(error.message); // Display error message
+      showAlert({
+        title: "Error",
+        message: error.message,
+        type: "error",
+      });
     } finally {
       setIsLoading(false);
     }
@@ -116,153 +146,159 @@ function LoginSignup() {
   };
 
   return (
-    <div className="login-signup">
-      <div className="container" ref={containerRef}>
-        <div className="form-box login">
-          <form onSubmit={handleLogin}>
-            <h1>Login</h1>
-            <div className="input-box">
-              <input
-                type="email"
-                name="email"
-                placeholder="Email"
-                value={formData.email}
-                onChange={handleInputChange}
-                required
-              />
-              <i className="fa fa-user"></i>
-            </div>
-            <div className="input-box">
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                value={formData.password}
-                onChange={handleInputChange}
-                required
-              />
-              <i className="fa fa-lock"></i>
-            </div>
-            <div className="remember-me">
-              <input
-                type="checkbox"
-                checked={rememberMe}
-                onChange={handleRememberMeChange}
-                id="rememberMe"
-              />
-              <label htmlFor="rememberMe">Remember Me</label>
-            </div>
-            <div className="forgot-link">
-              <a href="#">Forgot Password?</a>
-            </div>
-            <button type="submit" className="btn" disabled={isLoading}>
-              {isLoading ? <Spinner size={16} /> : "Login"}
-            </button>
-            <p>or register with social platforms</p>
-            <div className="social-icons">
-              <a href="#" className="google">
-                <i class="fa-brands fa-google"></i>
-              </a>
-              <a href="#" className="facebook">
-                <i className="fa-brands fa-facebook"></i>
-              </a>
-              <a href="#" className="github">
-                <i className="fa-brands fa-github"></i>
-              </a>
-              <a href="#" className="linkedin">
-                <i class="fa-brands fa-linkedin-in"></i>
-              </a>
-            </div>
-          </form>
-        </div>
-
-        <div className="form-box register">
-          <form onSubmit={handleRegister}>
-            <h1>Registration</h1>
-            <div className="input-box">
-              <input
-                type="text"
-                name="fullName"
-                placeholder="Full Name"
-                value={formData.fullName}
-                onChange={handleInputChange}
-                required
-              />
-              <i className="fa fa-user"></i>
-            </div>
-            <div className="input-box">
-              <input
-                type="email"
-                name="email"
-                placeholder="Email"
-                value={formData.email}
-                onChange={handleInputChange}
-                required
-              />
-              <i className="fa fa-envelope"></i>
-            </div>
-            <div className="input-box">
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                value={formData.password}
-                onChange={handleInputChange}
-                required
-              />
-              <i className="fa fa-lock"></i>
-            </div>
-            <div className="input-box">
-              <input
-                type="text"
-                name="phoneNumber"
-                placeholder="Phone Number"
-                value={formData.phoneNumber}
-                onChange={handleInputChange}
-                required
-              />
-              <i className="fa fa-phone"></i>
-            </div>
-            <button type="submit" className="btn" disabled={isLoading}>
-              {isLoading ? <Spinner size={16} /> : "Register"}
-            </button>
-            <p>or register with social platforms</p>
-            <div className="social-icons">
-              <a href="#" className="google">
-                <i class="fa-brands fa-google"></i>
-              </a>
-              <a href="#" className="facebook">
-                <i className="fa-brands fa-facebook"></i>
-              </a>
-              <a href="#" className="github">
-                <i className="fa-brands fa-github"></i>
-              </a>
-              <a href="#" className="linkedin">
-                <i class="fa-brands fa-linkedin-in"></i>
-              </a>
-            </div>
-          </form>
-        </div>
-
-        <div className="toggle-box">
-          <div className="toggle-panel toggle-left">
-            <h1>Hello, Welcome!</h1>
-            <p>Don't have an account?</p>
-            <button className="btn register-btn" ref={registerBtnRef}>
-              Register
-            </button>
+    <>
+      <Alert />
+      <div className="login-signup">
+        <div className="container" ref={containerRef}>
+          <div className="form-box login">
+            <form onSubmit={handleLogin}>
+              <h1>Login</h1>
+              <div className="input-box">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  required
+                />
+                <i className="fa fa-envelope"></i>
+              </div>
+              <div className="input-box">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  placeholder="Password"
+                  value={formData.password}
+                  onChange={handlePasswordChange}
+                  required
+                />
+                {isPasswordTyped && (
+                  <i
+                    className={`fa ${
+                      showPassword ? "fa-eye-slash" : "fa-eye"
+                    } password-toggle`}
+                    onClick={togglePasswordVisibility}
+                  ></i>
+                )}
+              </div>
+              <div className="remember-me">
+                <input
+                  type="checkbox"
+                  checked={rememberMe}
+                  onChange={handleRememberMeChange}
+                  id="rememberMe"
+                />
+                <label htmlFor="rememberMe">Remember Me</label>
+              </div>
+              <div className="forgot-link">
+                <a href="#">Forgot Password?</a>
+              </div>
+              <button type="submit" className="btn" disabled={isLoading}>
+                {isLoading ? <Spinner size={16} /> : "Login"}
+              </button>
+              <p>or register with social platforms</p>
+              <div className="social-icons">
+                <a href="#" className="google">
+                  <i class="fa-brands fa-google"></i>
+                </a>
+                <a href="#" className="facebook">
+                  <i className="fa-brands fa-facebook"></i>
+                </a>
+                <a href="#" className="github">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+                <a href="#" className="linkedin">
+                  <i class="fa-brands fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
           </div>
 
-          <div className="toggle-panel toggle-right">
-            <h1>Welcome Back!</h1>
-            <p>Already have an account?</p>
-            <button className="btn login-btn" ref={loginBtnRef}>
-              Login
-            </button>
+          <div className="form-box register">
+            <form onSubmit={handleRegister}>
+              <h1>Registration</h1>
+              <div className="input-box">
+                <input
+                  type="text"
+                  name="fullName"
+                  placeholder="Full Name"
+                  value={formData.fullName}
+                  onChange={handleInputChange}
+                  required
+                />
+                <i className="fa fa-user"></i>
+              </div>
+              <div className="input-box">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  required
+                />
+                <i className="fa fa-envelope"></i>
+              </div>
+              <div className="input-box">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  placeholder="Password"
+                  value={formData.password}
+                  onChange={handlePasswordChange}
+                  required
+                />
+                {isPasswordTyped && (
+                  <i
+                    className={`fa ${
+                      showPassword ? "fa-eye-slash" : "fa-eye"
+                    } password-toggle`}
+                    onClick={togglePasswordVisibility}
+                  ></i>
+                )}
+              </div>
+              <button type="submit" className="btn" disabled={isLoading}>
+                {isLoading ? <Spinner size={16} /> : "Register"}
+              </button>
+              <p>or register with social platforms</p>
+              <div className="social-icons">
+                <a href="#" className="google">
+                  <i class="fa-brands fa-google"></i>
+                </a>
+                <a href="#" className="facebook">
+                  <i className="fa-brands fa-facebook"></i>
+                </a>
+                <a href="#" className="github">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+                <a href="#" className="linkedin">
+                  <i class="fa-brands fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
+          </div>
+
+          <div className="toggle-box">
+            <div className="toggle-panel toggle-left">
+              <h1>Hello, Welcome!</h1>
+              <p>Don't have an account?</p>
+              <button className="btn register-btn" ref={registerBtnRef}>
+                Register
+              </button>
+            </div>
+
+            <div className="toggle-panel toggle-right">
+              <h1>Welcome Back!</h1>
+              <p>Already have an account?</p>
+              <button className="btn login-btn" ref={loginBtnRef}>
+                Login
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
